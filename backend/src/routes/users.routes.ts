6bb5d578bfea8a1bb52b66f485cab70c029f4e6a
Lucas <lsengenheirocivil@gmail.com>
2023-02-16 14:06:55 -0300

    feat/user-routes: cria rota de listagem de usuários

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import createUserController from "../controllers/users/createUser.controller";
+import listUsersController from "../controllers/users/listUsers.controller";
 import ensureEmailAlreadyExistMiddleware from "../middlewares/ensureEmailAlreadyExists.middleware";
 
 const userRoutes = Router();
 
 userRoutes.post("", ensureEmailAlreadyExistMiddleware, createUserController);
+userRoutes.get("", listUsersController);
 
 export default userRoutes;
