884a0e90cbd03496d57244474860d9681cac3528
Laís Martins <silveiramartinslais@gmail.com>
2023-03-06 07:26:50 -0300

    feat/components-section: adicionado efeito carrousel nas listas

--- frontend/src/components/Section/index.tsx
+++ frontend/src/components/Section/index.tsx
@@ -1,218 +1,253 @@
+import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 import { IAds } from "../../interfaces/IAds/IAds";
 import { IUser } from "../../interfaces/IUser/IUser";
 import Card from "../Card";
-import { CustonSection, EmptyList, ListCards, TitleSection } from "./style";
+import { CustonSection, EmptyList } from "./style";
 
 interface SectionProps {
-	id?: string;
-	userAd?: IUser;
-	titleSection: string;
-	value: string;
-	vehicles: IAds[];
-	auction: boolean;
-	advertiser?: boolean;
-	tags?: boolean;
-	profile?: boolean;
+  id?: string;
+  userAd?: IUser;
+  titleSection: string;
+  value: string;
+  vehicles: IAds[];
+  auction: boolean;
+  advertiser?: boolean;
+  tags?: boolean;
+  profile?: boolean;
 }
 
 const Section = ({
-	id,
-	userAd,
-	titleSection,
-	value,
-	vehicles,
-	auction,
-	advertiser,
-	tags,
-	profile,
+  id,
+  userAd,
+  titleSection,
+  value,
+  vehicles,
+  auction,
+  advertiser,
+  tags,
+  profile,
 }: SectionProps) => {
-	let hasList = false;
+  let hasList = false;
 
-	const conditionalEmptyList = () => {
-		switch (titleSection) {
-			case "Leilão":
-				return "Ainda não há leilões ativos 😢";
-			case "Carros":
-				return "Ainda não há carros cadastrados para venda 😢";
-			case "Motos":
-				return "Ainda não há motos cadastradas para venda 😢";
-		}
-	};
+  const carousel = useRef<null | HTMLDivElement>(null);
+  const [width, setWidth] = useState(0);
 
-	vehicles.forEach((vehicle: IAds) => {
-		if (
-			(auction && vehicle.typeAd === value && vehicle.isActive) ||
-			(!auction &&
-				vehicle.typeVehicle === value &&
-				vehicle.typeAd === "Venda" &&
-				vehicle.isActive) ||
-			(profile && auction && vehicle.typeAd === value) ||
-			(profile &&
-				!auction &&
-				vehicle.typeVehicle === value &&
-				vehicle.typeAd === "Venda")
-		) {
-			hasList = true;
-		}
-	});
+  useEffect(() => {
+    carousel.current &&
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
 
-	return (
-		<CustonSection id={id}>
-			<TitleSection>{titleSection}</TitleSection>
-			{hasList ? (
-				<>
-					{!profile && (
-						<ListCards>
-							{auction
-								? vehicles.map(
-										({
-											id,
-											user,
-											urlCoverImage,
-											title,
-											description,
-											mileage,
-											year,
-											price,
-											typeAd,
-											isActive,
-											typeVehicle,
-										}: IAds) =>
-											typeAd === value &&
-											isActive && (
-												<Card
-													key={id}
-													auction
-													id={id}
-													user={user}
-													urlCoverImage={
-														urlCoverImage
-													}
-													title={title}
-													description={description}
-													mileage={mileage}
-													year={year}
-													price={price}
-													advertiser={advertiser}
-													isActive={isActive}
-													tags={tags}
-												/>
-											)
-								  )
-								: vehicles.map(
-										({
-											id,
-											user,
-											urlCoverImage,
-											title,
-											description,
-											mileage,
-											year,
-											price,
-											typeAd,
-											typeVehicle,
-											isActive,
-										}: IAds) =>
-											typeVehicle === value &&
-											typeAd === "Venda" &&
-											isActive && (
-												<Card
-													key={id}
-													id={id}
-													user={user}
-													urlCoverImage={
-														urlCoverImage
-													}
-													title={title}
-													description={description}
-													mileage={mileage}
-													year={year}
-													price={price}
-													advertiser={advertiser}
-													isActive={isActive}
-													tags={tags}
-												/>
-											)
-								  )}
-						</ListCards>
-					)}
-					{profile && (
-						<ListCards>
-							{auction
-								? vehicles.map(
-										({
-											id,
-											urlCoverImage,
-											title,
-											description,
-											mileage,
-											year,
-											price,
-											typeAd,
-											isActive,
-											typeVehicle,
-										}: IAds) =>
-											typeAd === value && (
-												<Card
-													key={id}
-													auction
-													id={id}
-													user={userAd}
-													urlCoverImage={
-														urlCoverImage
-													}
-													title={title}
-													description={description}
-													mileage={mileage}
-													year={year}
-													price={price}
-													advertiser={advertiser}
-													isActive={isActive}
-													tags={tags}
-												/>
-											)
-								  )
-								: vehicles.map(
-										({
-											id,
-											urlCoverImage,
-											title,
-											description,
-											mileage,
-											year,
-											price,
-											typeVehicle,
-											isActive,
-											typeAd,
-										}: IAds) =>
-											typeVehicle === value &&
-											typeAd === "Venda" && (
-												<Card
-													key={id}
-													id={id}
-													user={userAd}
-													urlCoverImage={
-														urlCoverImage
-													}
-													title={title}
-													description={description}
-													mileage={mileage}
-													year={year}
-													price={price}
-													advertiser={advertiser}
-													isActive={isActive}
-													tags={tags}
-												/>
-											)
-								  )}
-						</ListCards>
-					)}
-				</>
-			) : (
-				<EmptyList>{conditionalEmptyList()}</EmptyList>
-			)}
-		</CustonSection>
-	);
+  const conditionalEmptyList = () => {
+    switch (titleSection) {
+      case "Leilão":
+        return "Ainda não há leilões ativos 😢";
+      case "Carros":
+        return "Ainda não há carros cadastrados para venda 😢";
+      case "Motos":
+        return "Ainda não há motos cadastradas para venda 😢";
+    }
+  };
+
+  vehicles.forEach((vehicle: IAds) => {
+    if (
+      (auction && vehicle.typeAd === value && vehicle.isActive) ||
+      (!auction &&
+        vehicle.typeVehicle === value &&
+        vehicle.typeAd === "Venda" &&
+        vehicle.isActive) ||
+      (profile && auction && vehicle.typeAd === value) ||
+      (profile &&
+        !auction &&
+        vehicle.typeVehicle === value &&
+        vehicle.typeAd === "Venda")
+    ) {
+      hasList = true;
+    }
+  });
+
+  return (
+    <CustonSection id={id}>
+      <motion.h5
+        className="title"
+        initial={{ x: -300 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        {titleSection}
+      </motion.h5>
+      {hasList ? (
+        <>
+          {!profile && (
+            <motion.div
+              ref={carousel}
+              className="carousel"
+              whileTap={{ cursor: "grabbing" }}
+            >
+              <motion.ul
+                className="inner"
+                drag="x"
+                dragConstraints={{ right: 0, left: -width }}
+                initial={{ x: 600 }}
+                animate={{ x: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                {auction
+                  ? vehicles.map(
+                      ({
+                        id,
+                        user,
+                        urlCoverImage,
+                        title,
+                        description,
+                        mileage,
+                        year,
+                        price,
+                        typeAd,
+                        isActive,
+                        typeVehicle,
+                      }: IAds) =>
+                        typeAd === value &&
+                        isActive && (
+                          <Card
+                            key={id}
+                            auction
+                            id={id}
+                            user={user}
+                            urlCoverImage={urlCoverImage}
+                            title={title}
+                            description={description}
+                            mileage={mileage}
+                            year={year}
+                            price={price}
+                            advertiser={advertiser}
+                            isActive={isActive}
+                            tags={tags}
+                          />
+                        )
+                    )
+                  : vehicles.map(
+                      ({
+                        id,
+                        user,
+                        urlCoverImage,
+                        title,
+                        description,
+                        mileage,
+                        year,
+                        price,
+                        typeAd,
+                        typeVehicle,
+                        isActive,
+                      }: IAds) =>
+                        typeVehicle === value &&
+                        typeAd === "Venda" &&
+                        isActive && (
+                          <Card
+                            key={id}
+                            id={id}
+                            user={user}
+                            urlCoverImage={urlCoverImage}
+                            title={title}
+                            description={description}
+                            mileage={mileage}
+                            year={year}
+                            price={price}
+                            advertiser={advertiser}
+                            isActive={isActive}
+                            tags={tags}
+                          />
+                        )
+                    )}
+              </motion.ul>
+            </motion.div>
+          )}
+          {profile && (
+            <motion.div
+              ref={carousel}
+              className="carousel"
+              whileTap={{ cursor: "grabbing" }}
+            >
+              <motion.ul
+                className="inner"
+                drag="x"
+                dragConstraints={{ right: 0, left: -width }}
+                initial={{ x: 600 }}
+                animate={{ x: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                {auction
+                  ? vehicles.map(
+                      ({
+                        id,
+                        urlCoverImage,
+                        title,
+                        description,
+                        mileage,
+                        year,
+                        price,
+                        typeAd,
+                        isActive,
+                        typeVehicle,
+                      }: IAds) =>
+                        typeAd === value && (
+                          <Card
+                            key={id}
+                            auction
+                            id={id}
+                            user={userAd}
+                            urlCoverImage={urlCoverImage}
+                            title={title}
+                            description={description}
+                            mileage={mileage}
+                            year={year}
+                            price={price}
+                            advertiser={advertiser}
+                            isActive={isActive}
+                            tags={tags}
+                          />
+                        )
+                    )
+                  : vehicles.map(
+                      ({
+                        id,
+                        urlCoverImage,
+                        title,
+                        description,
+                        mileage,
+                        year,
+                        price,
+                        typeVehicle,
+                        isActive,
+                        typeAd,
+                      }: IAds) =>
+                        typeVehicle === value &&
+                        typeAd === "Venda" && (
+                          <Card
+                            key={id}
+                            id={id}
+                            user={userAd}
+                            urlCoverImage={urlCoverImage}
+                            title={title}
+                            description={description}
+                            mileage={mileage}
+                            year={year}
+                            price={price}
+                            advertiser={advertiser}
+                            isActive={isActive}
+                            tags={tags}
+                          />
+                        )
+                    )}
+              </motion.ul>
+            </motion.div>
+          )}
+        </>
+      ) : (
+        <EmptyList>{conditionalEmptyList()}</EmptyList>
+      )}
+    </CustonSection>
+  );
 };
 
 export default Section;
--- frontend/src/components/Section/style.ts
+++ frontend/src/components/Section/style.ts
@@ -6,16 +6,38 @@ export const CustonSection = styled.section`
   flex-direction: column;
   /* gap: 40px; */
 
+  .title {
+    font-size: 24px;
+    font-weight: 600;
+    color: var(--grey0);
+  }
+
+  .carousel {
+    cursor: grab;
+    padding: 3rem 0;
+    gap: 12px;
+    overflow: hidden;
+
+    .inner {
+      display: flex;
+
+      @media screen and (min-width: 768px) {
+        gap: 24px;
+      }
+
+      .item {
+        min-height: 200px;
+        min-width: 400px;
+        padding: 14px;
+      }
+    }
+  }
+
   @media screen and (min-width: 1024px) {
     padding: 40px 0 40px 60px;
   }
 `;
 
-export const TitleSection = styled.h5`
-  font-size: 24px;
-  font-weight: 600;
-  color: black;
-`;
 export const ListCards = styled.ul`
   padding: 3rem 0;
   display: flex;
